5c45fab5ad82c7a963a769b6cb1318cc5d98b123
TimSousa <martimnevessousa@gmail.com>
2023-12-09 19:21:54 +0000

    auto script

--- build/run.sh
+++ build/run.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-# dir first argument
 
-for folder in $1/*; do
-for ((i=1; i<=$2; i*=2)); do
-    rm -r $folder/old_photo_PAR_A
-    echo running for $i threads
-    echo ./old-photo-paralelo-A $folder $i
-    ./old-photo-paralelo-A $folder $i
-done;
+for folder in $1/*
+do
+    echo rm $folder/*.csv
+    rm $folder/*.csv
+
+    for ((i=1; i<=$2; i*=2))
+    do
+        echo rm -r $folder/old_photo_PAR_A
+        rm -r $folder/old_photo_PAR_A
+
+        echo ./old-photo-paralelo-A $folder $i
+        ./old-photo-paralelo-A $folder $i
+    done
+echo awk '(NR == 1) || (FNR > 1)' $folder/*.csv > $folder.csv
+awk '(NR == 1) || (FNR > 1)' $folder/*.csv > $folder.csv
+
+echo rm $folder/timing*
+rm $folder/timing*
+
 done
-
-awk '(NR == 1) || (FNR > 1)' *.csv > out.csv
-rm timing*
